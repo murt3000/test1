b118503b0d853153f35a8475aaebeadc4ff9a3d1
doranj-wcsu <112014309+doranj-wcsu@users.noreply.github.com>
2022-09-15 07:17:11 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 from flask import Flask
+from flask import Flask,jsonify,request
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello World CS298- Jim!"
+
+  
+@app.route('/returnjson', methods = ['GET'])
+def ReturnJSON():
+    if(request.method == 'GET'):
+        data = {
+            "Modules" : 15,
+            "Subject" : "Data Structures and Algorithms",
+        }
+  
+        return jsonify(data)
+
+
 
 if __name__ == '__main__':
-    app.run(host="localhost",  port=8080 ,debug=True)
-
-    
+     app.run (host="0.0.0.0", port=8080)
